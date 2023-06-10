fab5f23b8626d8f4288aa1dfe1cd98a2b2f7df66
mehdi <midomoubarak24@gmail.com>
2023-06-10 23:24:45 +0100

    Initial commit

---
+++ laravel-back-end-crud-login/resources/views/Login.blade.php
@@ -0,0 +1,32 @@
+!<!DOCTYPE html>
+<head></head>
+<body>
+<form action="#" className="pt-3 m-auto">
+                    <div className="form-floating">
+                        <input type="email" className="form-control" id="email" placeholder="info@example.com"/>
+                        <label for="email">Email Address</label>
+                    </div>
+
+                    <div className="form-floating">
+                        <span className="password-show-toggle js-password-show-toggle"><span className="uil"></span></span>
+                        <input type="password" className="form-control" id="password" placeholder="Password"/>
+                        <label for="password">Password</label>
+                    </div>
+
+                    <div className="d-flex justify-content-between">
+                        <div className="form-check">
+                            <input type="checkbox" className="form-check-input" id="remember"/>
+                            <label for="remember" className="form-check-label">Keep me logged in</label>
+                        </div>
+                        <div><a href="#">Forgot password?</a></div>
+                    </div>
+                    
+                    <div className="d-grid mb-4">
+                        <button type="submit" className="btn btn-primary">Log in</button>
+                    </div>
+
+                    <div className="mb-2">Don’t have an account? <a href="signup.html">Sign up</a></div>
+
+
+                </form>
+                </body>
